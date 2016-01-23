7274e2214f8181e1720902ca3dd398c090b31e2a
cuixinkuan <cui19910119@163.com>
2016-01-23 10:47:47 +0800

    Initial Commit

---
+++ CCTouchIDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCTouchIDDemo
+//
+//  Created by admin on 16/1/23.
+//  Copyright © 2016年 CXK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCTouchIDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCTouchIDDemo
+//
+//  Created by admin on 16/1/23.
+//  Copyright © 2016年 CXK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
